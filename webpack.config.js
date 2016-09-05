9f8cc8a0bed357e74356ddf6aac6b667eeebacfe
Shah Muttaqi <shah@shahcreative.com>
2016-09-05 05:57:35 -0400

    Added plugins and environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,35 @@
 const webpack = require('webpack');
-const vendor_dir = __dirname + '/node_modules/';
+const debug = process.env.NODE_ENV !=="production";
 
 module.exports = {
+	context:__dirname,
+	devtool: debug ? "sourcemap" : null,
 	entry:{
 		app:['./src/index.js','./index.html'],
-		vendor:['react']
+		vendor:['react', 'react-dom']
 	},
-	resolve:{
-		alias:{
-			'react': vendor_dir + 'react/dist/react.js',
-			'react-dom': vendor_dir + 'react-dom/dist/react-dom.js'
-		}
-	},
-	plugins: [
-    	new webpack.optimize.CommonsChunkPlugin('vendor', 'app/js/vendors.js', Infinity)
-  	],
 	output:{
 		path:__dirname + "/dist",
 		filename:'app/js/main.js'
 	},
 	module:{
-		noParse:[vendor_dir + 'react/dist/react.js', vendor_dir + 'react-dom/dist/react-dom.js'],
 		loaders:[
 			{ test: /\.html$/, loader: "file?name=[name].[ext]" },
 			{ test: /\.(png|jpg|woff|woff2|eot|ttf|otf)/, loader: 'url-loader' },
 			{ test: /\.css$/, loader: "style-loader!css-loader" },
-            		{ test: /\.svg/, loader: 'file?name=/img/[hash].[ext]?' },
+            { test: /\.svg/, loader: 'file?name=/img/[hash].[ext]?' },
 			{ test:/\.jsx?$/, loader:'babel', exclude:/node_modules/ }
 		]
-	}
-}
+	},
+	plugins: debug ? [] : [
+    	new webpack.optimize.CommonsChunkPlugin('vendor', 'app/js/vendors.js', Infinity),
+    	new webpack.optimize.DedupePlugin(),
+    	new webpack.optimize.OccurenceOrderPlugin(),
+    	new webpack.optimize.UglifyJsPlugin({mangle:true, sourcemap:false, beautify:false, dead_code:true}),
+    	new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        })
+  ]
+};
